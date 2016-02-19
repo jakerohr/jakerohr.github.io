41ee574a360d61d4c3267ba9d9b01599a2a69fd3
Jake Rohr <jakerohr@gmail.com>
2016-02-19 00:14:27 -0800

    added stop and go, cross under

--- AstroTuff/script.js
+++ AstroTuff/script.js
@@ -11,27 +11,62 @@ function removeClass(el, className) {
     el.className=el.className.replace(reg, ' ')
   }
 }
-
-// Pick a play
-var play = "";
-$( "#play-one" ).click(function() {
-  $( "#pre-snap").attr("id", "in-n-out")
-  play = "in-n-out";
-  $("#title").text("In-N-Out")
-});
-
-var set = true
-var goBtn = document.getElementById("go-btn");
-goBtn.onclick = hikeBall
-
-function hikeBall () {
   var qb = document.getElementById("qb");
   var one = document.getElementById("one");
   var two = document.getElementById("two");
   var three = document.getElementById("three");
   var four = document.getElementById("four");
   var five = document.getElementById("five");
+  var ball = document.getElementById("ball");
+
   var team = [qb,one,two,three,four,five];
+// Pick a play
+var set = false
+var play = "";
+$( "#play-picker" ).click(function() {
+  console.log($( ".pre-snap").attr("id"))
+  if ($( ".pre-snap").attr("id") !== "pre-snap") {
+    set = false
+      for (var i = 0; i < team.length; i++) {
+        addClass(team[i], "fa-circle-o")
+        removeClass(team[i], "hike");
+        removeClass(team[i], team[i].classList[1]);
+      }
+  }
+  console.log(set)
+  play = "";
+  // $("#title").text("In-N-Out")
+});
+$( "#play-one" ).click(function() {
+  $( ".pre-snap").attr("id", "in-n-out")
+  play = "in-n-out";
+  set = true
+  $("#title").text("In-N-Out")
+  goBtn.innerHTML = "Hike!";
+});
+$( "#play-two" ).click(function() {
+  $( ".pre-snap").attr("id", "stop-and-go")
+  play = "stop-and-go";
+  set = true
+  $("#title").text("Stop and Go")
+  goBtn.innerHTML = "Hike!";
+});
+$( "#play-three" ).click(function() {
+  $( ".pre-snap").attr("id", "cross-under")
+  play = "cross-under";
+  set = true
+  $("#title").text("Cross Under")
+  goBtn.innerHTML = "Hike!";
+});
+
+var goBtn = document.getElementById("go-btn");
+goBtn.onclick = hikeBall
+
+function hikeBall () {
+
+  console.log(play);
+  console.log("set: ", set);
+
   if (play == "") {
     $("#title").text("Please pick a play!");
   } else {
@@ -46,10 +81,20 @@ function hikeBall () {
           removeClass(five, "fa-child")
           addClass(qb, "fa-user")
           removeClass(qb, "fa-child")
+        } else if (play == "stop-and-go") {
+          addClass(team[i], "fa-child")
+          removeClass(team[i], "fa-circle-o")
+          addClass(qb, "fa-user")
+          removeClass(qb, "fa-child")
+        } else if (play == "cross-under") {
+          addClass(team[i], "fa-shield")
+          removeClass(team[i], "fa-circle-o")
+          addClass(qb, "fa-user")
+          removeClass(qb, "fa-shield")
+          addClass(five, "fa-child")
+          removeClass(five, "fa-shield")
         }
       }
-
-
       goBtn.innerHTML = "Reset";
       set = false
 
@@ -59,7 +104,7 @@ function hikeBall () {
         removeClass(team[i], "hike");
         removeClass(team[i], team[i].classList[1])
       }
-      console.log("qb classes: ",qb.classList)
+      console.log("set: ",set)
       goBtn.innerHTML = "Hike!";
       set = true
     }
